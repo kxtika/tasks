f6535a1da8fc9539dda1fe4e771f2b83c61fc2ca
kxtika <93083691+kxtika@users.noreply.github.com>
2021-10-24 22:14:36 +0300

    Create hometask1to4.js

---
+++ hometask1to4.js
@@ -0,0 +1,47 @@
+// 1. Знайти площу прямокутника.
+
+let length = prompt('Enter the length of your rectangle')
+let width = prompt('Enter the width of your rectangle')
+
+function area (length, width) {
+return length * width;
+}
+
+alert('The area of your rectangle is ' + area(length, width));
+
+// 2. Дана функція squareCubeSum. Вона вимагає першим параметром число, другим функцію, котра підносить число до квадрату, а третім параметром функцію, яка підносить до куба.
+// Ці функції є як Function Declaration - squared, cubed. Нехай функція squareCubeSum поверне суму квадрата и куба числа.
+
+let yourNum = prompt('Enter your number');
+
+function squared() {
+    return yourNum**2;
+};
+
+function cubed() {
+    return yourNum**3;
+}
+
+function squareCubeSum(yourNum, squared, cubed) {
+alert ('The sum is ' + (squared(yourNum) + cubed(yourNum)));
+};
+
+squareCubeSum(yourNum, squared, cubed);
+
+// 3. Написати функцію, яка буде нескінченно збільшувати 1 число(prompt) на 2 число(prompt)
+
+let num1 = +prompt('Enter first number');
+let num2 = +prompt('Enter second number');
+let sum = num1 + num2;
+alert (sum);
+function infinitePlus(){ 
+return sum += num2; 
+}
+while (true) {  
+ alert ('Next time you click the button, this number ' + infinitePlus() + ' will be increased by the second number you have entered before');
+}
+
+// 4. 
+
+
+        
